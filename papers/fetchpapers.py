02704ad2b6e7a2d71943cacfd41e087a5a75e529
Johannes Leppäkorpi <user3610@protonmail.com>
2021-10-17 16:08:37 +0300

    Indexed HL Physics papers

--- papers/fetchpapers.py
+++ papers/fetchpapers.py
@@ -1,15 +1,35 @@
-# A script to fetch a bunch of papers from
-# https://www.ibdocuments.com/IB%20PAST%20PAPERS%20-%20SUBJECT/Group%204%20-%20Sciences/Physics_HL/
+# A script to sort the papers
+import os
+from collections import defaultdict
 
 class TheFetcher:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, subject):
+        self.dir = [os.path.join(dp, f) for dp, dn, filenames in
+                    os.walk("/aus/jo/bitconnect/papers/{}".format(subject)) for f in filenames if
+                    os.path.splitext(f)[1] == '.pdf']
+        self.year = defaultdict(list)
+        self.papers = {}
 
     def fetch(self):
-        print(self.url)
+        print("Total number of files: ", len(self.dir))
+        # Parse the useless files out
+        for subject in self.dir:
+            if "Spanish" or "French" or "German" in subject:
+                self.dir.remove(subject)
+        print("After parsing: ", len(self.dir))
+
+        # Assign each question to the corresponding markscheme
+        x = 0
+        for subject in self.dir:
+            year = subject.split("/")[10].split(" ")[0]
+            self.year[year].append(subject)
+            x+=1
+            # if x == 5:
+            #     exit()
+        print(len(self.year))
 
 if __name__ == "__main__":
-    urls = ["https://www.ibdocuments.com/IB%20PAST%20PAPERS%20-%20SUBJECT/Group%204%20-%20Sciences/Physics_HL/"]
+    subjects = ["Physics"]
 
-    for url in urls:
-        TheFetcher(url).fetch()
+    for subject in subjects:
+        TheFetcher(subject).fetch()
