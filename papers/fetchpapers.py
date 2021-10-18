0e24b5a74b7813fb6271fe837d18a9483f445f84
Johannes Leppäkorpi <user3610@protonmail.com>
2021-10-18 07:08:32 +0300

    Indexed SL_Physics papers

--- papers/fetchpapers.py
+++ papers/fetchpapers.py
@@ -5,8 +5,9 @@ from collections import defaultdict
 class TheFetcher:
     def __init__(self, subject):
         self.dir = [os.path.join(dp, f) for dp, dn, filenames in
-                    os.walk("/aus/jo/bitconnect/papers/{}".format(subject)) for f in filenames if
+                    os.walk("/aus/jo/IBeanos/papers/{}".format(subject)) for f in filenames if
                     os.path.splitext(f)[1] == '.pdf']
+        self.allPapers = []
         self.year = defaultdict(list)
         self.papers = {}
 
@@ -14,19 +15,15 @@ class TheFetcher:
         print("Total number of files: ", len(self.dir))
         # Parse the useless files out
         for subject in self.dir:
-            if "Spanish" or "French" or "German" in subject:
-                self.dir.remove(subject)
-        print("After parsing: ", len(self.dir))
+            if ("Spanish" or "French") not in subject:
+                self.allPapers.append(subject)
+        print("After parsing: ", len(self.allPapers))
 
         # Assign each question to the corresponding markscheme
-        x = 0
-        for subject in self.dir:
+        for subject in self.allPapers:
             year = subject.split("/")[10].split(" ")[0]
             self.year[year].append(subject)
-            x+=1
-            # if x == 5:
-            #     exit()
-        print(len(self.year))
+        print(len(self.year["2002"]))
 
 if __name__ == "__main__":
     subjects = ["Physics"]
