8e19e3f093750b849f9703f9f9e06b8be34a5a16
Johannes Leppäkorpi <user3610@protonmail.com>
2022-02-04 14:09:55 +0200

    React ting

---
+++ ibeanos/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1><a href="/hl">Physics HL</a></h1>
+        <h1><a href="/sl">Physics SL</a></h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ibeanos/src/components/sl.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+import "../App.css";
+
+export default class Sl extends Component {
+    constructor() {
+        super();
+    }
+
+    render(){
+        // Render the paper and timer
+        return(
+            <div className="App">
+                <header className="App-header">
+                    <h1>Here is the SL paper</h1>
+                </header>            
+            </div>        
+        )
+    }
+}
